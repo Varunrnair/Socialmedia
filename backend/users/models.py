8f2279fb6489d00fc3ebb880f74e6d1a08e5dfd5
Varun <varunr.nair@outlook.com>
2023-04-20 12:23:32 +0530

    added user login/registration features

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    objects = CustomUserManager()
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_short_name(self):
+        return self.first_name
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .serializers import *
+from .models import *
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth import authenticate, login, logout
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+
+class RegisterView(APIView):
+    # authentication_classes=[JWTAuthentication]
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({'status': 'success', 'user_id': user.id})
+        else:
+            return Response(serializer.errors, status=400)
+
+class LoginView(APIView):
+    authentication_classes=[JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                return Response({'status': 'success'})
+            else:
+                return Response({'status': 'failed', 'message': 'Invalid credentials'}, status=401)
+        else:
+            return Response(serializer.errors, status=400)
+        
+class display(APIView):
+    serializer_class=UserSerializer
+    def get(self,request):
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+        
